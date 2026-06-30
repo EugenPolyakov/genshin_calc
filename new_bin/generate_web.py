936ad97ce797369fa35d4445d5cc3d7ff71dbf35
EugenPolyakov <GothicI@yandex.ru>
2026-06-30 22:11:50 +0300

    Исправляем actions

--- new_bin/generate_web.py
+++ new_bin/generate_web.py
@@ -5,29 +5,11 @@ sys.path.insert(0, dirname)
 import time
 start_time = time.time()
 
-import re
-import logging
-
-logger = logging.getLogger(__name__)
-
-# do_single = False
-# if len(sys.argv) > 1:
-#     for arg in sys.argv[1:3]:
-#         if arg == '-single':
-#             do_single = True
-#         else:
-#             generate_char = arg
-
-# if do_single and not generate_char:
-#     print('error')
-#     sys.exit()
 out_dir = os.path.join('', dirname, '../src/js/generated/');
 os.makedirs(out_dir, exist_ok= True)
 
 def processFiles(dr, rel, outfile):
     for d in os.scandir(dr):
-        print(d.name)
-        print(d.path)
         if d.is_dir():
             processFiles(d.path + '/', rel + d.name + '/', outfile)
         elif d.is_file():
